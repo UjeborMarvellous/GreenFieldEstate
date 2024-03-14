1fed350b652858854be0f91bc0f08bdc399c82f4
UjeborMarvellous <ujebormarvellous@gmail.com>
2024-03-14 20:42:40 +0100

    Slide

---
+++ src/Components/slider/Slider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Slider() {
+  return (
+    <div>
+        <div className="slide">
+            <h1>Sliding Window Algorithm</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Slider
